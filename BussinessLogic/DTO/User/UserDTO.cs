e7c2a813733e538093e8e80abcc091a47b5a2457
Tutul Chakma <tutulcou@gmail.com>
2024-08-03 01:31:29 +0600

    Commit

--- BussinessLogic/DTO/User/UserDTO.cs
+++ BussinessLogic/DTO/User/UserDTO.cs
@@ -1,10 +1,13 @@
 ﻿using Shared;
+using System.ComponentModel.DataAnnotations;
 
 namespace BussinessLogic
 {
     public class UserDTO:User
     {
+        [Required]
         public string UserName { get; set; } = "";
+        [Required]
         public string Password { get; set; } = "";
     }
 }
