cda215915581353b138151314682730ca4c724ee
Tutul Chakma <tutulcou@gmail.com>
2024-08-03 00:24:12 +0600

    commit

--- BussinessLogic/DataRepository/Repository/User/UserRepository.cs
+++ BussinessLogic/DataRepository/Repository/User/UserRepository.cs
@@ -21,54 +21,70 @@ namespace BussinessLogic.DataRepository
 
         public Task<bool> Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public async Task<UserDTO> Save(UserDTO User)
         {
-            var MaxID = _context.AdUsers.DefaultIfEmpty().Max(x => x == null ? 0 : x.Id) + 1;
-            var _mapper = new MapperConfiguration(cfg =>
+            try
             {
-                cfg.CreateMap<UserDTO, AdUser>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => MaxID))
-                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src =>DateTime.Now ))
-                .ForMember(dest => dest.LastUpdate, opt => opt.MapFrom(src => DateTime.Now))
-                .ForMember(dest => dest.LastUpdatedBy, opt => opt.MapFrom(src => 1))
-                .BeforeMap((src, dest) =>
+                var MaxID = _context.AdUsers.DefaultIfEmpty().Max(x => x == null ? 0 : x.Id) + 1;
+                var _mapper = new MapperConfiguration(cfg =>
                 {
-                    //Code Here ,if Needed
-                })
-                .AfterMap((src, dest) =>
-                {
-                    src.Id = dest.Id;
-                    src.IsActive = dest.IsActive;
-                    src.CreatedDate = dest.CreatedDate;
-                    src.LastUpdate = dest.LastUpdate;
-                    src.LastUpdatedBy = dest.LastUpdatedBy;
-                })
-                ;
-            }).CreateMapper();
+                    cfg.CreateMap<UserDTO, AdUser>()
+                    .ForMember(dest => dest.Id, opt => opt.MapFrom(src => MaxID))
+                    .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+                    .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTime.Now))
+                    .ForMember(dest => dest.LastUpdate, opt => opt.MapFrom(src => DateTime.Now))
+                    .ForMember(dest => dest.LastUpdatedBy, opt => opt.MapFrom(src => 1))
+                    .BeforeMap((src, dest) =>
+                    {
+                        //Code Here ,if Needed
+                    })
+                    .AfterMap((src, dest) =>
+                    {
+                        src.Id = dest.Id;
+                        src.IsActive = dest.IsActive;
+                        src.CreatedDate = dest.CreatedDate;
+                        src.LastUpdate = dest.LastUpdate;
+                        src.LastUpdatedBy = dest.LastUpdatedBy;
+                    })
+                    ;
+                }).CreateMapper();
 
-            var user = _mapper.Map<AdUser>(User);
+                var user = _mapper.Map<AdUser>(User);
 
-            _context.Add(user);
-           
-           
-            var MaxID2 = _context.AdUserLogins.DefaultIfEmpty().Max(x => x == null ? 0 : x.Id) + 1;
-            AdUserLogin login = new AdUserLogin();
-            login.Id = MaxID2;
-            login.UserId = MaxID;
-            login.UserName = User.UserName;
-            login.Password = User.Password;
-            login.IsActive = true;
-            login.CreatedDate = DateTime.Now;
-            login.LastUpdate = DateTime.Now;
-            _context.Add(login);
+                _context.Add(user);
 
-            await _context.SaveChangesAsync();
-            
-            return User;
+
+                var MaxID2 = _context.AdUserLogins.DefaultIfEmpty().Max(x => x == null ? 0 : x.Id) + 1;
+                AdUserLogin login = new AdUserLogin();
+                login.Id = MaxID2;
+                login.UserId = MaxID;
+                login.UserName = User.UserName;
+                login.Password = User.Password;
+                login.IsActive = true;
+                login.CreatedDate = DateTime.Now;
+                login.LastUpdate = DateTime.Now;
+                _context.Add(login);
+
+                await _context.SaveChangesAsync();
+
+                return User;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
         public async Task<UserDTO> UserLogin(UserLoginDTO User)
         {
@@ -93,7 +109,6 @@ namespace BussinessLogic.DataRepository
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
         }
